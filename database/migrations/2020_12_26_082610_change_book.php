653e97c0e4ac19ae5ebcd53aadbb2e7c57bcc4d3
bad4iz <develop@bad4iz.ru>
2020-12-26 13:19:43 +0400

    GraphQL w/ Laravel & Vue - Lighthouse Package Backend - Part 2

---
+++ database/migrations/2020_12_26_082610_change_book.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeBook extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('title');
+            $table->string('author');
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->string('link')->nullable();
+            $table->boolean('featured')->default(false);
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
+    }
+}
